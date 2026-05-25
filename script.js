9bdf93218e617bc5ecbc851dd832166aba155918
Jpzibarth <joaopedrozibarth@gmail.com>
2026-05-25 08:13:59 -0300

    k

--- script.js
+++ script.js
@@ -77,7 +77,6 @@ function podiumMedalhas(pontos) {
         caixaPerguntas.textContent = "Resultado da competição: 4 pontos é PRATA!";
     } else if (pontos === 5) {
         caixaPrincipal.style.backgroundImage = "url('img/ouro.png')";
-        // Corrigido o texto abaixo de Bronze para OURO
         caixaPerguntas.textContent = "Resultado da competição: 5 pontos é OURO!"; 
     } else if (pontos < 3) {
         caixaPrincipal.style.backgroundImage = "url('img/perdeu.png')";
